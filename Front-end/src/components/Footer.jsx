0ca44dab81cdd27298edd9de93c3463d86cf96a9
zizile <zizilelucia@gmail.com>
2024-01-18 15:23:12 +0200

    Working on the footer

--- Front-end/src/components/Footer.jsx
+++ Front-end/src/components/Footer.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="mt-8 w-full bg-black px-8 md:px-[500px] flex justify-between text-sm md:text-md py-8">
+      <div className="flex flex-col text-white">
+        <p>Featured Blogs</p>
+        <p>Most viewed</p>
+        <p>Readers choice</p>
+      </div>
+      <div className="flex flex-col text-white">
+        <p>Forum</p>
+        <p>Support</p>
+        <p>Recent Posts</p>
+      </div>
+      <div className="flex flex-col text-white">
+        <p>Privacy Policy</p>
+        <p>About us</p>
+        <p>Terms and conditions</p>
+      </div>
+    </div>
   )
 }
 
--- Front-end/src/pages/Home.jsx
+++ Front-end/src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import HomePost from "../components/HomePost"
 
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="px-8 md:px-[200px]">
+      <HomePost/>
+    </div>
   )
 }
 
