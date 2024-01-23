fb846ea8815d5caee9b263f60e54b627b99e7184
zizile <zizilelucia@gmail.com>
2024-01-23 08:43:57 +0200

    Working on the PostDetails page

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,22 +1,24 @@
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
+// import Footer from "./components/Footer"
+// import Navbar from "./components/Navbar"
 import {Route, Routes} from 'react-router-dom'
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import PostDetails from './pages/PostDetails'
 
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/register" element={<Register/>}/>
+        <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
 
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   )
 }
---
+++ Front-end/src/pages/PostDetails.jsx
@@ -0,0 +1,35 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import {BiEdit} from 'react-icons/bi'
+import {MdDelete} from 'react-icons/md'
+
+
+function PostDetails() {
+  return (
+    <div>
+        <Navbar/>
+        <div className="px-8-[200px] mt-8">
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl font-bold text-black md:text-3xl">Medicine is important</h1>
+            <div className="flex items-center justify-center space-x-2">
+                <p><BiEdit/></p>
+                <p><MdDelete/></p>
+
+            </div>
+            </div>
+        <div className="flex items-center justify-between mt-2 md:mt-4">
+        <p>Zizi</p>
+            <div className="flex space-x-2">
+            <p>1/18/2024</p> 
+            <p>14:35</p>
+
+        </div>
+        </div>
+        <img src ="" className="" alt=""/>
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default PostDetails
