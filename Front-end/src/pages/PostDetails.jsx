4549998f15ffac98f5968e62c7965a5b0bd080e5
zizile <zizilelucia@gmail.com>
2024-01-24 14:36:30 +0200

    Write a comment

--- Front-end/src/pages/PostDetails.jsx
+++ Front-end/src/pages/PostDetails.jsx
@@ -38,47 +38,50 @@ function PostDetails() {
         <div className="flex flex-col mt-4">
             <h3 className="mt-6 mb-4 font-semibold">Comments:</h3>
             {/* Comments section */}
-            <div className="px-2 py-2 bg-gray-200 rounded-lg">
+            <div className="px-2 py-2 bg-gray-200 rounded-lg my-2">
             <div className="flex items-center justify-between">
                 <h3 className="font-bold text-red-600">Aphiwe</h3>
                 <div className="flex justify-center items-center space-x-4">
                  <p className="text-gray-500 text-sm">24/01/2024</p>
                  <p className="text-gray-500 text-sm">11:23</p>
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            <div className="flex items-center justify-center space-x-2">
+                <p><BiEdit/></p>
+                <p><MdDelete/></p>
+
+            </div>
+            </div>
+            </div>
+            <p className="px-4 mt-2"> Educative information</p>
+            </div>
+
+             {/* Comments section */}
+             <div className="px-2 py-2 bg-gray-200 rounded-lg my-2">
+            <div className="flex items-center justify-between">
+                <h3 className="font-bold text-red-600">Aphiwe</h3>
+                <div className="flex justify-center items-center space-x-4">
+                 <p className="text-gray-500 text-sm">24/01/2024</p>
+                 <p className="text-gray-500 text-sm">11:23</p>
             
             <div className="flex items-center justify-center space-x-2">
                 <p><BiEdit/></p>
                 <p><MdDelete/></p>
 
             </div>
-
-                    </div>
-
-                </div>
-
+            </div>
+            </div>
+            <p className="px-4 mt-2"> Nice information</p>
             </div>
         </div>
+
+        {/* Write a comment */}
+        <div className="w-full flex flex-col mt-4 md:flex-row">
+            <input type="text" placeholder="Write a comment" className="md:w-[90%] outline-none px-4 mt-4 md:mt-0"/>
+            <button className="bg-black text-sm text-white px-2 py-2 md:w-[10%] mt-4 md:mt-0">Add comment</button>
+
+
+        </div>
+
         </div>
         <Footer/>
     </div>
