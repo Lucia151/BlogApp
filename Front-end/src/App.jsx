dda3a2f2e77fab57481ec84b0fee7b4ac7e305c5
zizile <zizilelucia@gmail.com>
2024-01-24 16:21:54 +0200

    Where you can create posts

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import PostDetails from './pages/PostDetails'
+import CreatePost from './pages/CreatePost'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/register" element={<Register/>}/>
+        <Route exact path="/write" element={<CreatePost/>}/>
         <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
 
       </Routes>
---
+++ Front-end/src/pages/CreatePost.jsx
@@ -0,0 +1,17 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+
+function CreatePost() {
+  return (
+    <div>
+        <Navbar/>
+        <div>
+            
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default CreatePost
