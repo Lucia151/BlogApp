44466d72f613ec7e6563ea0a2b293f9a94d9ac62
zizile <zizilelucia@gmail.com>
2024-02-05 14:55:11 +0200

    added foopter

--- Front-end/src/pages/Register.jsx
+++ Front-end/src/pages/Register.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../assets/images/logo.png';
+import Footer from '../components/Footer';
 
 function Register() {
   return (
     <>
-      <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-        <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">My Blog</Link></h1>
+   
+      <div className="sticky top-0 z-50 flex items-center justify-between px-6 md:px-[200px] py-4 shadow-md shadow-blue-gray-900/200">
+      <Link to="/"><img className='h-20 w-30' src={logo} /></Link>
         <h3><Link to="/login">Login</Link></h3>
         </div>
-    <div className="w-full flex justify-center items-center h-[70vh]">
+        <div className="bg-gray-100 mt-5 ">
+    <form className="w-full flex justify-center items-center h-[70vh]">
         <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
             <h1 className="text-xl font-bold text-left">Create an account</h1>
-            <input className="w-full px-4 py-2 border-2 border-black outline-0" type="password" placeholder="Enter your username"/>
-            <input className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your email"/> 
-            <input className="w-full px-4 py-2 border-2 border-black outline-0" type="password" placeholder="Enter your password"/>  
-            <button className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black">Register</button>
+            <input className="w-full px-4 py-2 border-2 border-inputOutlineGray outline-0" type="password" placeholder="Enter your username"/>
+            <input className="w-full px-4 py-2 border-2 border-inputOutlineGray outline-0" type="text" placeholder="Enter your email"/> 
+            <input className="w-full px-4 py-2 border-2 border-inputOutlineGray outline-0" type="password" placeholder="Enter your password"/>  
+            <button className="w-full px-4 py-4 text-lg font-bold text-white bg-buttonsBlue rounded-lg hover:bg-gray-500 hover:text-black">Register</button>
             <div className="flex justify-center items-center space-x-4">
              <p>Already have an account?</p>
-             <p className="text-gray-500 hover:text-black"><Link to="/login">Login</Link></p>
+             <p className="text-buttonsBlue hover:text-black"><Link to="/login">Login</Link></p>
 
             </div>
 
         </div>
 
+    </form>
+    
     </div>
+    <p className="py-2 pb-2 text-center text-white bg-black"></p>
+    <p className="py-2 pb-2 text-center text-white bg-black">All rights reserved @My blog 2024</p>
     </>
+
   )
 }
 
